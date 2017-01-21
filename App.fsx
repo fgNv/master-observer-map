05545ef8853ad859e8915c635efade7d4fe162ab
fgUnderlost <felipe.underlost@gmail.com>
2017-01-21 12:34:39 -0200

    adjusting initialize script

--- App.fsx
+++ App.fsx
@@ -25,66 +25,12 @@
 #load "MasterObserverMap/SignalRConfiguration.fs"
 #load "MasterObserverMap/Routes.fs"
 
-#r "System.dll"
-#r "mscorlib"
-#r "System.Core.dll"
-
 open Suave
 open System
 
-open System
-open System.Security.Cryptography.X509Certificates
-open System.Net.Security
-open System.Net
-
-let tempContentFolder = "AssetsTemp"
-
-if Directory.Exists(tempContentFolder) then
-    Directory.Delete(tempContentFolder, true)
-
-Directory.CreateDirectory(tempContentFolder)
-
-File.Copy(Path.Combine("MasterObserverMap", "index.html"), Path.Combine(tempContentFolder, "index.html"))
-let scripts = Directory.GetFiles(Path.Combine("MasterObserverMap", "Scripts"))
-Directory.CreateDirectory(Path.Combine(tempContentFolder, "Scripts"))
-scripts |> Seq.iter (fun s -> File.Copy(s, Path.Combine(tempContentFolder, "Scripts", Path.GetFileName s)))
-
-let myHomeFolder = Path.Combine(__SOURCE_DIRECTORY__, tempContentFolder)
+let myHomeFolder = Path.Combine(__SOURCE_DIRECTORY__, "MasterObserverMap")
 let cfg = { defaultConfig with homeFolder = Some(myHomeFolder) }
-
-let delegation (o : Object) 
-               (certificate : X509Certificate) 
-               (chain : X509Chain) 
-               (errors : SslPolicyErrors) = 
-    try
-        System.Console.WriteLine("verifying certificate")
-        match errors with
-            | SslPolicyErrors.None -> 
-                System.Console.WriteLine("no error")
-                true
-            | _ -> System.Console.WriteLine("verifying errors")
-                   chain.ChainStatus |> 
-                   Seq.fold (fun acc (c : X509ChainStatus) -> 
-                        if not <| acc then 
-                            false 
-                        else
-                            chain.ChainPolicy.RevocationFlag <- X509RevocationFlag.EntireChain
-                            chain.ChainPolicy.RevocationMode <- X509RevocationMode.Online
-                            chain.ChainPolicy.UrlRetrievalTimeout <- new TimeSpan (0, 1, 0)
-                            chain.ChainPolicy.VerificationFlags <- X509VerificationFlags.AllFlags
-                            chain.Build (certificate :?> X509Certificate2) ) true 
-    with
-        | error ->
-            System.Console.WriteLine("expcetion occurred -> " + error.Message)                                    
-            false
-
-let certificateValidationCallback = RemoteCertificateValidationCallback(delegation) 
-
-ServicePointManager.ServerCertificateValidationCallback <- certificateValidationCallback
-
-System.Console.WriteLine("before launch service")
+Console.WriteLine("before launch service")
 
 startWebServer cfg Routes.app
 
-File.Delete(Path.Combine(tempContentFolder, "index.html"))
-Directory.Delete(Path.Combine(tempContentFolder, "Scripts"))
