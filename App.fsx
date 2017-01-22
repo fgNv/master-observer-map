98c1b9aa91f14fb50ae38592130f8f28c3cc0344
fgNv <planetary_confinement@hotmail.com>
2017-01-22 13:10:05 -0200

    adding folder references

--- App.fsx
+++ App.fsx
@@ -1,11 +1,28 @@
-﻿#r "packages/Owin/lib/net40/Owin.dll"
+﻿#I "packages/Owin/lib/net40"
+#r "packages/Owin/lib/net40/Owin.dll"
+
+#I "packages/Suave/lib/net40"
 #r "packages/Suave/lib/net40/Suave.dll"
+
+#I "packages/Microsoft.AspNet.SignalR.Core/lib/net45"
 #r "packages/Microsoft.AspNet.SignalR.Core/lib/net45/Microsoft.AspNet.SignalR.Core.dll"
+
+#I "packages/Microsoft.AspNet.SignalR.SystemWeb/lib/net45"
 #r "packages/Microsoft.AspNet.SignalR.SystemWeb/lib/net45/Microsoft.AspNet.SignalR.SystemWeb.dll"
+
+#I "packages/Microsoft.Owin.Host.SystemWeb/lib/net45"
 #r "packages/Microsoft.Owin.Host.SystemWeb/lib/net45/Microsoft.Owin.Host.SystemWeb.dll"
+
+#I "packages/Microsoft.Owin.Security/lib/net45"
 #r "packages/Microsoft.Owin.Security/lib/net45/Microsoft.Owin.Security.dll"
+
+#I "packages/Microsoft.AspNet.SignalR.Core/lib/net45"
 #r "packages/Microsoft.AspNet.SignalR.Core/lib/net45/Microsoft.AspNet.SignalR.Core.dll"
+
+#I "packages/Newtonsoft.Json/lib/net45"
 #r "packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+
+#I "packages/Microsoft.Owin/lib/net45"
 #r "packages/Microsoft.Owin/lib/net45/Microsoft.Owin.dll"
 
 #load "MasterObserverMap/Domain.fs"
